d3a7c0069112af8cfeba6d6e3f4532c78c43b7c7
knalli <knallisworld@googlemail.com>
2019-12-07 11:05:40 +0100

    🔌Day 7: Amplification Circuit (Part 1)

---
+++ day07/main.go
@@ -0,0 +1,298 @@
+package main
+
+import (
+	dl "de.knallisworld/aoc/aoc2019/dayless"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+const AocDay = 7
+const AocDayName = "day07"
+const AocDayTitle = "Day 07"
+
+func main() {
+	dl.PrintDayHeader(AocDay, AocDayTitle)
+	defer dl.TimeTrack(time.Now(), AocDayName)
+
+	{
+		dl.PrintStepHeader(1)
+		puzzle := readFileAsIntArray(AocDayName + "/puzzle1.txt")
+		max := findHighestSignal(puzzle, []int{0, 1, 2, 3, 4})
+		dl.PrintSolution(fmt.Sprintf("The highest signal is: %d", max))
+	}
+
+	dl.PrintStepHeader(2)
+	dl.PrintSolution("Not solved yet")
+
+}
+
+func findHighestSignal(puzzle []int, phaseSequence []int) int {
+	max := 0
+	for _, phaseSequences := range permutations(phaseSequence) {
+		out := compute(puzzle, phaseSequences, 0)
+		if out > max {
+			max = out
+		}
+	}
+	return max
+}
+
+func permutations(arr []int) [][]int {
+	result := make([][]int, 0)
+	for p := make([]int, len(arr)); p[0] < len(p); nextPerm(p) {
+		result = append(result, getPerm(arr, p))
+	}
+	return result
+}
+
+func nextPerm(p []int) {
+	for i := len(p) - 1; i >= 0; i-- {
+		if i == 0 || p[i] < len(p)-i-1 {
+			p[i]++
+			return
+		}
+		p[i] = 0
+	}
+}
+
+func getPerm(orig, p []int) []int {
+	result := append([]int{}, orig...)
+	for i, v := range p {
+		result[i], result[i+v] = result[i+v], result[i]
+	}
+	return result
+}
+
+func readFileAsIntArray(file string) []int {
+	str, _ := dl.ReadFileToString(file)
+	arr := strings.Split(strings.TrimSpace(*str), ",")
+	return dl.ParseStringToIntArray(arr)
+}
+
+func compute(instructions []int, phaseSequences [] int, input int) int {
+	io := input
+	for _, phase := range phaseSequences {
+		data := make([]int, len(instructions))
+		for i, v := range instructions {
+			data[i] = v
+		}
+		tio, halt := executionInstructions(data, []int{phase, io}, false)
+		if halt == nil {
+			panic("Program not halted correctly")
+		}
+		io = tio
+	}
+	return io
+}
+
+// copy of day5/compute2
+func executionInstructions(data []int, inputs []int, debug bool) (int, error) {
+	usedInputs := 0
+	outputs := make([]int, 0)
+	i := 0
+	for {
+		iOpcode := data[i]
+		opcode := iOpcode
+		if opcode > 10000 {
+			opcode %= 10000
+		}
+		if opcode > 1000 {
+			opcode %= 1000
+		}
+		if opcode > 100 {
+			opcode %= 100
+		}
+		mode01 := (iOpcode / 100) % 10
+		mode02 := (iOpcode / 1000) % 10
+		//mode03 := (iOpcode / 10000) % 10
+		switch opcode {
+		case 1:
+			{
+				var param1, param2 int
+				var param1s, param2s string
+				if mode01 == 1 {
+					param1 = data[i+1]
+					param1s = fmt.Sprintf("%d", data[i+1])
+				} else {
+					param1 = data[data[i+1]]
+					param1s = fmt.Sprintf("#%d", data[i+1])
+				}
+				if mode02 == 1 {
+					param2 = data[i+2]
+					param2s = fmt.Sprintf("%d", data[i+2])
+				} else {
+					param2 = data[data[i+2]]
+					param2s = fmt.Sprintf("#%d", data[i+2])
+				}
+				data[data[i+3]] = param1 + param2
+				if debug {
+					fmt.Printf("[%05d] ADD %s %s => #%d\n", i, param1s, param2s, data[i+3])
+				}
+				i += 4
+			}
+		case 2:
+			{
+				var param1, param2 int
+				var param1s, param2s string
+				if mode01 == 1 {
+					param1 = data[i+1]
+					param1s = fmt.Sprintf("%d", data[i+1])
+				} else {
+					param1 = data[data[i+1]]
+					param1s = fmt.Sprintf("#%d", data[i+1])
+				}
+				if mode02 == 1 {
+					param2 = data[i+2]
+					param2s = fmt.Sprintf("%d", data[i+2])
+				} else {
+					param2 = data[data[i+2]]
+					param2s = fmt.Sprintf("#%d", data[i+2])
+				}
+				data[data[i+3]] = param1 * param2
+				if debug {
+					fmt.Printf("[%05d] MUL %s %s => #%d\n", i, param1s, param2s, data[i+3])
+				}
+				i += 4
+			}
+		case 3:
+			{
+				data[data[i+1]] = inputs[usedInputs]
+				if debug {
+					fmt.Printf("[%05d] IN %d => #%d\n", i, inputs[usedInputs], data[i+1])
+				}
+				usedInputs++
+				i += 2
+			}
+		case 4:
+			{
+				var param1 int
+				if mode01 == 1 {
+					param1 = data[i+1]
+				} else {
+					param1 = data[data[i+1]]
+				}
+				outputs = append(outputs, param1)
+				if debug {
+					fmt.Printf("[%05d] OUT #%d\n", i, data[i+1])
+				}
+				i += 2
+			}
+		case 5: // jump-if-true
+			{
+				var param1, param2 int
+				var param1s, param2s string
+				if mode01 == 1 {
+					param1 = data[i+1]
+					param1s = fmt.Sprintf("%d", data[i+1])
+				} else {
+					param1 = data[data[i+1]]
+					param1s = fmt.Sprintf("#%d", data[i+1])
+				}
+				if mode02 == 1 {
+					param2 = data[i+2]
+					param2s = fmt.Sprintf("%d", data[i+2])
+				} else {
+					param2 = data[data[i+2]]
+					param2s = fmt.Sprintf("#%d", data[i+2])
+				}
+				if debug {
+					fmt.Printf("[%05d] JIT %s %s\n", i, param1s, param2s)
+				}
+				if param1 != 0 {
+					i = param2
+				} else {
+					i += 3
+				}
+			}
+		case 6: // jump-if-false
+			{
+				var param1, param2 int
+				var param1s, param2s string
+				if mode01 == 1 {
+					param1 = data[i+1]
+					param1s = fmt.Sprintf("%d", data[i+1])
+				} else {
+					param1 = data[data[i+1]]
+					param1s = fmt.Sprintf("#%d", data[i+1])
+				}
+				if mode02 == 1 {
+					param2 = data[i+2]
+					param2s = fmt.Sprintf("%d", data[i+2])
+				} else {
+					param2 = data[data[i+2]]
+					param2s = fmt.Sprintf("#%d", data[i+2])
+				}
+				if debug {
+					fmt.Printf("[%05d] JIF %s %s\n", i, param1s, param2s)
+				}
+				if param1 == 0 {
+					i = param2
+				} else {
+					i += 3
+				}
+			}
+		case 7: // less-than
+			{
+				var param1, param2 int
+				var param1s, param2s string
+				if mode01 == 1 {
+					param1 = data[i+1]
+					param1s = fmt.Sprintf("%d", data[i+1])
+				} else {
+					param1 = data[data[i+1]]
+					param1s = fmt.Sprintf("#%d", data[i+1])
+				}
+				if mode02 == 1 {
+					param2 = data[i+2]
+					param2s = fmt.Sprintf("%d", data[i+2])
+				} else {
+					param2 = data[data[i+2]]
+					param2s = fmt.Sprintf("#%d", data[i+2])
+				}
+				if debug {
+					fmt.Printf("[%05d] SLT %s %s => #%d\n", i, param1s, param2s, data[i+3])
+				}
+				if param1 < param2 {
+					data[data[i+3]] = 1
+				} else {
+					data[data[i+3]] = 0
+				}
+				i += 4
+			}
+		case 8: // equals
+			{
+				var param1, param2 int
+				var param1s, param2s string
+				if mode01 == 1 {
+					param1 = data[i+1]
+					param1s = fmt.Sprintf("%d", data[i+1])
+				} else {
+					param1 = data[data[i+1]]
+					param1s = fmt.Sprintf("#%d", data[i+1])
+				}
+				if mode02 == 1 {
+					param2 = data[i+2]
+					param2s = fmt.Sprintf("%d", data[i+2])
+				} else {
+					param2 = data[data[i+2]]
+					param2s = fmt.Sprintf("#%d", data[i+2])
+				}
+				if debug {
+					fmt.Printf("[%05d] SIE %s %s => #%d\n", i, param1s, param2s, data[i+3])
+				}
+				if param1 == param2 {
+					data[data[i+3]] = 1
+				} else {
+					data[data[i+3]] = 0
+				}
+				i += 4
+			}
+		case 99:
+			return outputs[len(outputs)-1], errors.New("got Halt And Catch Fire")
+		default:
+			panic(fmt.Sprintf("invalid op code %d (%d)", opcode, iOpcode))
+		}
+	}
+}
---
+++ day07/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import "testing"
+
+func Test_compute(t *testing.T) {
+	type args struct {
+		instructions   []int
+		phaseSequences [] int
+		input          int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Sample1",
+			args: args{
+				instructions:   []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+				phaseSequences: []int{4, 3, 2, 1, 0},
+				input:          0,
+			},
+			want: 43210,
+		},
+		{
+			name: "Sample2",
+			args: args{
+				instructions:   []int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+				phaseSequences: []int{0, 1, 2, 3, 4},
+				input:          0,
+			},
+			want: 54321,
+		},
+		{
+			name: "Sample3",
+			args: args{
+				instructions:   []int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+				phaseSequences: []int{1, 0, 4, 3, 2},
+				input:          0,
+			},
+			want: 65210,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := compute(tt.args.instructions, tt.args.phaseSequences, tt.args.input); got != tt.want {
+				t.Errorf("compute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findHighestSignal(t *testing.T) {
+	type args struct {
+		puzzle        []int
+		phaseSequence []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Puzzle1",
+			args: args{
+				puzzle:        readFileAsIntArray("puzzle1.txt"),
+				phaseSequence: []int{0, 1, 2, 3, 4},
+			},
+			want: 46248,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findHighestSignal(tt.args.puzzle, tt.args.phaseSequence); got != tt.want {
+				t.Errorf("findHighestSignal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
